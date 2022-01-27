f612acd72a4369954813c49970434a3c7234f8fb
Firdaus Alam Hudi <alanfirdaus999@gmail.com>
2022-01-27 15:54:19 +0700

    Elements

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,5 +1,57 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 export default function Button(props) {
+  const className = [props.clasName]
+  if(props.isPrimary) className.push("btn-primary")
+  if(props.isLarge) className.push("btn-lg")
+  if(props.isSmall) className.push("btn-sm")
+  if(props.isBlock) className.push("btn-block")
+  if(props.hasShadow) className.push("btn-shadow")
+
+  const onclick = () => {
+    if(props.onClick) props.onClick()
+  }
+
+  if(props.type === "link") {
+    if(props.isExternal){
+      return(
+        <a 
+          href={props.href} 
+          clasName={className.join(" ")} 
+          style={props.style} 
+          target={props.target ==="_blank" ? "_blank":undefined} 
+          rel={props.target ==="_blank" ? "noopener noreferrer":undefined}
+          >
+          {props.children}</a>
+          
+        </a>
+      )
+    }else{
+      return(
+        <link href={props.href}
+        clasName={className.join(" ")}
+        style={props.style}>
+          {props.children}
+        </link>
+      )
+    }
+  }
   return <div></div>;
 }
+
+Button.propTypes = {
+  type: propTypes.onOf(['button', 'link']),
+  onclick: propTypes.func,
+  href: propTypes.string,
+  target: propTypes.string,
+  className: propTypes.string,
+  isDisabled: propTypes.bool,
+  isLoading: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  isExternal: propTypes.bool,
+  hasShadow: propTypes.bool,
+};
---
+++ src/pages/LandingPages.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Component } from 'elements/Button';
+
+export default function LandingPages() {
+  return <div></div>;
+}
